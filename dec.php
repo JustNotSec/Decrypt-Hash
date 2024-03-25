6816562aaa1f3c917c1a601b419958280423e2aa
Abdul Malik <141676888+JustNotSec@users.noreply.github.com>
2024-03-26 00:47:10 +0700

    Update dec.php

--- dec.php
+++ dec.php
@@ -3,14 +3,21 @@ error_reporting(0);
 
 # Coded by L0c4lh34rtz - IndoXploit
 
-if(isset($argv[1])) {
+if(isset($argv[1]) && isset($argv[2])) {
     $wordlist_file = $argv[1];
+    $output_file = "ok.txt"; // File to store OK hashes
     if(file_exists($wordlist_file)) {
         $wordlist = file($wordlist_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         $hashes = explode("\n", file_get_contents($argv[2])); // Load hashes from file
         foreach($hashes as $hash) {
             foreach($wordlist as $word) {
-                print " [+]"; print (password_verify($word, $hash)) ? "$hash -> $word (OK)\n" : "$hash -> $word (SALAH)\n";
+                if (password_verify($word, $hash)) {
+                    print " [+] $hash -> $word (OK)\n";
+                    // Write OK hashes to output file
+                    file_put_contents($output_file, "$hash -> $word\n", FILE_APPEND);
+                } else {
+                    print " [+] $hash -> $word (SALAH)\n";
+                }
             }
         }
     } else {
